90a728a9a1ff8a80ad60feacff1fdaa5ad351b56
Pablo Gonzalez <pabloasecas@gmail.com>
2016-10-22 11:35:24 -0400

    The basic node and problem classes

--- Node.java
+++ Node.java
@@ -1,4 +1,82 @@
+//This class represents a node in the search tree
+//Each instance of a Node includes:
+// parent, The node that generated this one
+// action: The action applied to the parent node to generate it
+// state: a representation of the problem state
+// cost: the cost up to this node
+// estimated: the estimated cost to the solution
 
 public class Node {
-
+	private int cost;
+	private int estimateh;
+	private Node parent;
+	private int[][] state;
+	private String action;
+	private Problem problem;
+	private Boolean root;
+	 
+	//Constructor for the node class
+	public Node (Node parentIN,String actionIN,int[][] stateIN,Problem problemIN ){
+		setParent(parentIN);
+		setAction(actionIN);
+		setState(stateIN);
+		problem = problemIN;
+	}
+	public Node (int[][] stateIN,Problem problemIN ){
+		setRoot(true);
+		setParent(null);
+		setAction(null);
+		setState(stateIN);
+		this.problem = problemIN;
+	}
+	
+	public Node[] generateSuccessorList (){
+		Node[] list = problem.generateSuccessorList (this);
+		
+		return list;
+		
+		
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public int getEstimateh() {
+		return estimateh;
+	}
+	public void setEstimateh(int estimateh) {
+		this.estimateh = estimateh;
+	}
+	public Node getParent() {
+		return parent;
+	}
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public Boolean getRoot() {
+		return root;
+	}
+	public void setRoot(Boolean root) {
+		this.root = root;
+	}
+	public int[][] getState() {
+		return state;
+	}
+	public void setState(int[][] state) {
+		this.state = state;
+	}
+	
+	
+	
+	
+	
 }
--- Problem.java
+++ Problem.java
@@ -1,4 +1,24 @@
 
-public class Problem {
-
+public abstract class Problem {
+	public String name;
+	
+	public Problem (String name){
+		this.name=name;
+	}
+	
+	
+	public abstract Node[] generateSuccessorList (Node parent);
+	
+	//Takes a node and an action and executes the action on the node
+	//Input: parent, the node to execute the action on
+	//Input: action a String defining the action
+	//Output: the new node
+	public abstract Node executeAction (Node parent,String action);
+	
+	public abstract Boolean testGoal (Node targetNode);
+	
+	public abstract void printState(int[][] state);
+	public abstract Node getStartNode();
+	
+	
 }
