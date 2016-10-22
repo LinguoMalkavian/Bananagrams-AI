6c29699462e8861fa433b129b4ce76c6511d2d79
Pablo Gonzalez <pabloasecas@gmail.com>
2016-10-22 11:38:08 -0400

    Added a simple tester class with a basic setting to test the Puzzle node generator and tester

--- Tester.java
+++ Tester.java
@@ -2,8 +2,23 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		testNpuzzle();
+		
+	}
+	
+	public static void testNpuzzle(){
+		String npuz="1,2,3,4,5,6,7,9,8";
+		Problem theProblem = new NpuzzleProblem("eight puzzle",8,npuz);
+		
+		theProblem.printState(theProblem.getStartNode().getState());
+		Node[] successors=theProblem.generateSuccessorList(theProblem.getStartNode());
+		for (int i =0; i<successors.length;i++){
+			System.out.println("---------------");
+			theProblem.printState(successors[i].getState());
+			Boolean solved= theProblem.testGoal(successors[i]);
+			System.out.println(solved);
+		}
 	}
 
 }
