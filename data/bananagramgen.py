41528fd5008bc3a3e9f365e4f5de92fd3bc46371
Pablo <pabloasecas@gmail.com>
2016-11-07 21:55:24 -0500

    Finish bananagram generator and generate sample problems of several sizes

--- data/bananagramgen.py
+++ data/bananagramgen.py
@@ -1,48 +1,69 @@
 
 from math import *
 from random import random
-from collections import DefaultDict
+from collections import defaultdict
 
 def generateBProblem(order, outfile):
+	print "Generating %d-grams"%order
 	tiles=[]
+	counter=0
 	while len(tiles)<order :
-             
-		randomind = int(floor(random()*len(dictionary)))
-		randomword = dictionary[randomind]
-		if len(randomword)-1<order-len(tiles):
-			if tiles==[]:
-				lastword=randomword
-				for letter in randomword: 
+		remaining=order-len(tiles)
+		if tiles==[]:
+			randomlength= int( floor(1 + random()*(order-2)))
+		elif remaining<4:
+			randomlength=remaining
+
+		thisDict=limitedDictionaries[randomlength+1]
+
+		randomind = int(floor(random()*len(thisDict)))
+
+		randomword = thisDict[randomind]
+		if tiles==[]:
+			lastword=randomword
+			oldinter=-2;
+			for letter in randomword:
+				tiles.append(letter)
+		else:
+			inter1,inter2,letter=findintersection(lastword,randomword)
+			if inter1!=-1 and abs(inter1-oldinter)>1:
+				randomword=randomword[:inter2]+randomword[inter2+1:]
+				for letter in randomword:
 					tiles.append(letter)
-			else:
-				inter1,inter2,letter=findintersection(lastword,randomword)
-				if inter1!=-1 and abs(inter1-oldinter)>1:
-					randomword.pop(inter2)
-					for letter in randomword:
-						tiles.append(letter)
-					lastword=randomword
-					oldinter=inter2
-		
+				lastword=randomword
+				oldinter=inter2
+		if counter>10000:
+			counter=0
 			tiles=[]
-	outfile.write(",".join(tiles))
+		counter+=1
+	outfile.write(",".join(tiles)+"\n")
 
 
 def findintersection(word1, word2):
 	for i in range(len(word1)):
 		for j in range(len(word2)):
 			if word1[i]==word2[j]:
-				return i,j,word[i]
+				return i,j,word1[i]
 	return -1,-1,""
-def main():
-    infile= open("dictionary.txt",'r')
-    dictionary=infile.readlines()
-    infile.close()
-    limitedDictionaries=DefaultDict(lambda:[])
-    for word in dictionary:
-        limitedDictionaries[len(word)].append(word)
-    for order in range(4,15):	
-    	filename = "problems/%s-bananagramProblems.txt"%(order) 
-    	outfile=file(filename,'w')
-    	for i in range(20):
-    		generateBProblem(order,outfile)
-    	outfile.close()
+
+infile= open("dictionary.txt",'r')
+dictionary=infile.readlines()
+infile.close()
+limitedDictionaries=defaultdict(lambda:[])
+for word in dictionary:
+	limitedDictionaries[len(word.strip())].append(word.strip())
+for dictio in limitedDictionaries:
+	print "Dictionary of order %d has %d entries"%(dictio,len(limitedDictionaries[dictio]))
+
+for order in range(15,20):
+	filename = "problems/%s-bananagramProblems.txt"%(order)
+	outfile=file(filename,'w')
+	if order<6:
+		probnum=50
+	elif order<10:
+		probnum=20
+	else:
+		probnum=5
+	for i in range(probnum):
+		generateBProblem(order,outfile)
+	outfile.close()
